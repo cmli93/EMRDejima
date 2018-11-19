ca8cdb114edec2f8f295d60e046dca8312b13f56
cmli93 <chunmiaoli1993@gmail.com>
2018-11-19 19:13:39 +0900

    query and update succeed

--- contracts/queryMeta.sol
+++ contracts/queryMeta.sol
@@ -11,8 +11,10 @@ contract queryMeta {
 
     //Store EMRMetas
     mapping (uint => EMRMeta) public EMRMetas;
+    //store EMRMetas count
+    uint public EMRMetasCount;
 
-    struct Role{
+    struct User{
       string name; //name of doctor or patient
       uint id;
       uint roleNum; /*
@@ -25,7 +27,15 @@ contract queryMeta {
     }
 
     //Store　roles
-    mapping (uint => Role) public roles;
+    mapping (uint => User) public users;
+    //store EMRMetas count
+    uint public usersCount;
+
+    //update EMRMeta event
+    event updateMetaEvent (
+        uint indexed _owner
+    );
+
 
     /*
      * Contructor, will run whenever we deploy the contract to the blockchain
@@ -33,15 +43,95 @@ contract queryMeta {
      * same name as the contract
     */
     function queryMeta () public {
-           //先直接初始化一些roles
-           roles[0] = Role("Alice", 888, 1); //doctor
-           roles[1] = Role("Bob", 87, 4); //a patient
+           //initialize users directly
+           //users[0] = User("Alice", 888, 1); //doctor
+           //users[1] = User("Bob", 87, 4); //a patient
 
-           //先直接初始化一些EMRMetas
-           EMRMetas[0] = EMRMeta(123, "20181118", 1);
-           EMRMetas[1] = EMRMeta(456, "20181119", 4);
+           addUser("Alice", 888, 1);//doctor
+           addUser("Bob", 87, 4);//a patient
+
+           //initialize EMRMetas directly
+           //EMRMetas[0] = EMRMeta(123, "20181118", 1);
+           //EMRMetas[1] = EMRMeta(456, "20181119", 4);
+
+           addMetas(123, "20181118", 1);
+           addMetas(456, "20181119", 4);
     }
 
-    //Read EMRMetas
+    //add a new medical metadata
+    function addMetas (uint _owner, string _timestamp, uint _allowedRole) private{
+        //addMetas　string _timestamp can be get automatically later (solidity date)
 
+        //check role to be allowed to addEMRMetas
+        //require(roles[msg.sender] == 1); //only a doctor can addEMRMetas now
+        //here msg.sender is an address, msg.sender to identify the address of who calling this function
+
+        /********************************************************************************/
+        /*******Here we should consider to add "medical data" to cloud (dejima)**********/
+        /********************************************************************************/
+
+        EMRMetasCount ++;
+
+        EMRMetas[EMRMetasCount] = EMRMeta(_owner, _timestamp, _allowedRole);
+
+    }
+
+    //add a new person who belongs a role
+    function addUser (string _name, uint _id, uint _roleNum) private
+    {
+      //check the priviledge to add User
+
+      // add users in the cloud
+
+      usersCount ++;
+
+      users[usersCount] = User(_name, _id, _roleNum);
+
+    }
+
+    //update an existing medical metadata, suppose will change the allowedRole
+    function updateMetas (uint _owner) public
+    {
+        //updateMetas　string _timestamp can be get automatically later
+
+        //check role to be allowed to updateEMRMetas
+        //require(roles[msg.sender] == 1); //only a doctor can updateEMRMetas now
+        //here msg.sender is an address, msg.sender to identify the address of who calling this function
+
+        /********************************************************************************/
+        /*******Here we should consider to update "medical data" to cloud (dejima)**********/
+        /********************************************************************************/
+
+        for (uint i = 1; i <= EMRMetasCount; i++)
+        {
+           if(EMRMetas[i].owner == _owner)
+             EMRMetas[i].allowedRole = 3;
+        }
+
+        // trigger updateMeta　Event
+        updateMetaEvent(_owner);
+    }
+
+    /*deleteEMRMetas string _timestamp can be get automatically later
+    function deleteEMRMetas (uint _owner, string _timestamp) private{
+
+        //check role to be allowed to deleteEMRMetas
+        require(roles[msg.sender] == 1); //only a doctor can deleteEMRMetas now
+
+        /********************************************************************************/
+        /*******Here we should consider to delete "medical data" to cloud (dejima)*******/
+        /********************************************************************************/
+
+        /*
+          If a metadata is deleted, the owner ID is assigned to 0
+          (if this, later will need to fresh EMRMetas to remove unuseful EMRMetas)
+
+
+        //search EMRMetas using _owner
+
+        //assign 0 to this _owner field
+
+        //decrement the EMRMetasCount by 1
+
+    }*/
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,138 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
-    return await App.initWeb3();
+  init: function() {
+    return App.initWeb3(); //to initialize app
   },
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
+  initWeb3: function() { //initializes connection of our client application to our local blockchain
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+  //loads up our contract into founded application so we can interact with it.
+  initContract: function()
+  {
+    $.getJSON("queryMeta.json", function(queryMeta)
+    {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.queryMeta = TruffleContract(queryMeta);
+      // Connect provider to interact with contract
+      App.contracts.queryMeta.setProvider(App.web3Provider);
 
-    return App.bindEvents();
+      App.listenForEvents(); //call listenForEvents
+
+      return App.render();
+    });
   },
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  //Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.queryMeta.deployed().then(function(instance) {
+      // Restart Chrome if you are unable to receive this event
+      // This is a known issue with Metamask
+      // https://github.com/MetaMask/metamask-extension/issues/2393
+      instance.updateMetaEvent({}, {
+        fromBlock: 0,
+        toBlock: 'latest'
+      }).watch(function(error, event) {
+        console.log("event triggered", event)
+        // Reload when a new update is recorded
+        App.render();
+      });
+    });
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  //layout all content on the page
+  render: function()
+  {
+    var queryMetaInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    // a lite-server in package.json
+    web3.eth.getCoinbase(function(err, account)
+    {
+      if (err === null)
+      {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    // Load contract data (list all items to the front side)
+    App.contracts.queryMeta.deployed().then(function(instance)
+    {
+      queryMetaInstance = instance;
+      return queryMetaInstance.EMRMetasCount();
+    }).then(function(EMRMetasCount)
+    {
+      var EMRMetasResults = $("#EMRMetasResults");
+      EMRMetasResults.empty();
+
+      var EMRMetaSelect = $('#EMRMetaSelect');
+      EMRMetaSelect.empty();
+
+      for (var i = 1; i <= EMRMetasCount; i++)
+      {
+        queryMetaInstance.EMRMetas(i).then(function(EMRMeta)
+        {
+          var owner = EMRMeta[0];
+          var timestamp = EMRMeta[1];
+          var allowedRole = EMRMeta[2];
+
+          // Render EMRMetas Result
+          var EMRMetaTemplate = "<tr><th>" + owner + "</th><td>" + timestamp + "</td><td>" + allowedRole + "</td></tr>"
+          EMRMetasResults.append(EMRMetaTemplate);
+
+          // Render EMRMetas records to update
+          var EMRMetaOption = "<option value ='" + owner + "' >" + owner + "</ option>"
+          EMRMetaSelect.append(EMRMetaOption);
+       });
+      }
+
+      loader.hide();
+      content.show();
+    });
   },
 
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
+  updateMeta: function()
+  {
+    var metaId = $('#EMRMetaSelect').val();
+    App.contracts.queryMeta.deployed().then(function(instance)
+    {
+      return instance.updateMetas(metaId, { from: App.account });
+    }).then(function(result)
+    {
+      // Wait for Meta update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err)
+    {
+      console.error(err);
+    });
   }
-
 };
 
-$(function() {
-  $(window).load(function() {
+$(function()
+{
+  $(window).load(function()
+  {
     App.init();
   });
 });
